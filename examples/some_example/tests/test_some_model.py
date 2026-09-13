4a502f2f37809d6e16429125dfc87c6217712afa
fsodari <franksodari@gmail.com>
2026-09-13 13:38:40 -0700

    ...

--- examples/some_example/tests/test_some_model.py
+++ examples/some_example/tests/test_some_model.py
@@ -4,16 +4,18 @@ from some_example.library import SomeModel
 
 
 def test_some_model():
+    assert SomeModel.DW == 24
+
     with Context() as ctx:
         with ctx.construct():
             clk = Clock(10, name="clk")
             rst = Dff8(clk, 1, name="rst")
             i = Signal32(SomeModel.DW)
             o = Signal32(SomeModel.DW)
-            model = SomeModel(clk=clk, rst=rst, i=i, o=o)
+            some_model = SomeModel(clk, rst, i, o, name="some_model")
 
         # ctx.set_timescale("1ns", "1ns")
-        model.trace(Path("traces/some_model.vcd"))
+        some_model.trace(Path("traces/some_model.vcd"))
 
         rst.d = 1
         ctx.run(100)
